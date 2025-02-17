e94d39757f362c27605591fba869ddb2fd4845e1
ktyntang <ktyntang@hotmail.com>
2025-02-17 20:31:56 +0800

    fix: remove https server

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,6 @@
 import dotenv from 'dotenv';
-import fs from 'fs';
 import { createServer as createHttpServer, Server as HttpServer } from 'http';
-import { createServer as createHttpsServer, Server as HttpsServer } from 'https';
-import path from 'path';
+import { Server as HttpsServer } from 'https';
 
 import { app } from '@/server';
 import { logger } from '@/shared/utils/logger';
@@ -13,15 +11,6 @@ let server: HttpServer | HttpsServer;
 const port = process.env.PORT || 3000;
 
 if (process.env.NODE_ENV === 'development') {
-    const options = {
-        key: fs.readFileSync(path.join(__dirname, '/shared/certs/localhost-key.pem')),
-        cert: fs.readFileSync(path.join(__dirname, '/shared/certs/localhost.pem')),
-    };
-    server = createHttpsServer(options, app);
-    server.listen(port, () => {
-        logger.info(`[server]: Server is running at https://localhost:${port}`);
-    });
-} else {
     server = createHttpServer(app);
     server.listen(port, () => {
         logger.info(`[server]: Server is running at http://localhost:${port}`);
--- src/server.ts
+++ src/server.ts
@@ -14,7 +14,7 @@ const app: Express = express();
 // Security Middlewares
 app.use(
     cors({
-        origin: ['https://localhost:3000'],
+        origin: ['http://localhost:5173'],
         credentials: true,
         methods: ['GET', 'POST', 'PUT', 'DELETE'],
         allowedHeaders: ['Content-Type', 'Authorization', 'Origin', 'X-Requested-With', 'Accept'],
@@ -28,6 +28,7 @@ app.use(express.urlencoded({ extended: false }));
 
 // TODO : add rate limiter
 // TODO : add session middleware
+// TODO : cacheing
 
 // Logging Requests
 app.use(httpLogger);
